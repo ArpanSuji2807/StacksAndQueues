239f178fbed74c0cf2f5d938a823ee21ad49512d
Praan26 <arpanap102@gmail.com>
2022-05-02 19:52:10 +0530

    [Arpan] Add: Stack is being is created

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace StacksAndQueues;
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to stacks and queues program");
+        LinkedListStacks list = new LinkedListStacks();
+        bool check = true;
+        Console.WriteLine("1.Stack Creation");
+        while(check)
+        {
+            Console.WriteLine("Take an option to execute: ");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    list.Push(56);
+                    list.Push(30);
+                    list.Push(70);
+                    list.Display();
+                    break;
+                case 2:
+                    Console.WriteLine("Enter a valid choice");
+                    break;
+                    default:break;
+            }
+        }
+    }
+}
